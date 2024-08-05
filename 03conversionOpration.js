266303bd952f4f24fada2ec20dcdf2da2b2e1ef9
Ahmad Hamesh <ahmadhamesh4@gmail.com>
2024-08-05 19:34:57 +0530

    Conversion operations

--- 03conversionOpration.js
+++ 03conversionOpration.js
@@ -30,6 +30,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber) //33
-console.log(typeof stringNumber) //string
+// console.log(stringNumber) //33
+// console.log(typeof stringNumber) //string
+
+
+/*  ************** operations *************  */
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " hamesh"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2) //12
+// console.log(1 + "2") //12
+// console.log("1" + 2 + 2) //122
+// console.log(1 + 2 + "2") //32
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter;
+console.log(gameCounter++) //postfix
+console.log(++gameCounter) //prefix
 
