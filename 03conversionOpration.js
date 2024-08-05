d53b8f7fc6e57d4fe79b7c69baa3a7d4f0a255b7
Ahmad Hamesh <ahmadhamesh4@gmail.com>
2024-08-05 18:45:10 +0530

    Conversion of datatypes

---
+++ 03conversionOpration.js
@@ -0,0 +1,35 @@
+let score = "Hamesh"
+
+// console.log(typeof score)
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*notes
+when we change the value string to number then output
+"33" => 33
+"33sbc" => NaN 
+true => 1; false => 0
+*/
+
+let isLoggedIn = "Hamesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+/*notes
+1 => true; 0 => false; 
+"" => false
+"Hamesh" => true
+
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber) //33
+console.log(typeof stringNumber) //string
+
