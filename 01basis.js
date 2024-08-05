cf9a0a01d3e3b6c9e3fdff13d756a7b404febfd4
Ahmad Hamesh <ahmadhamesh4@gmail.com>
2024-08-05 14:03:03 +0530

    DATATYPES

---
+++ 01basis.js
@@ -0,0 +1,26 @@
+//let var const
+
+const accountId = 143232
+let accountEmail = "ahmad12@gmil.com"
+var accountPassword = "12345"
+accountCity = "Uttar Pradesh" //you can use this to reserve memory but this is not correct way
+
+let accountState
+//accountId = 2 //not allowed
+
+
+accountEmail = "ah123@gmail.com"
+accountPassword = "212121"
+accountCity = "Delhi"
+
+/*
+Prefer not to use var
+because of issue in block and functional scope
+
+
+*/
+
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
---
+++ 02datatype.js
@@ -0,0 +1,36 @@
+"use strict" //treat all js code as a newer version
+
+
+//alert(3+3) // we are using node js, not browser , bcoz inside browser have js engine, whatever we write in console is a we wrirte inside document
+
+console.log(3
+    +
+    3)//code readablity should be high
+
+console.log("Hamesh")
+
+
+let name = "Hamesh"
+let age = 28
+let isLoggedIn = false
+
+/*
+primitive data type
+number=> 2 to power 53 this is a range that can use memory address  
+bigint => use to store large number
+string => "" use to write inside a quote
+boolean => true/false
+null=> standlone value(empty)
+//undefined => defined a variable but not defined value
+symbols => unique
+*/
+
+/*
+Non Primitive data type
+Object
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
