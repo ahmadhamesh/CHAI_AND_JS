34d448f17eab0c8d360932b17af9fd1bf6759303
Ahmad Hamesh <ahmadhamesh4@gmail.com>
2024-09-23 16:42:14 +0530

    projects 1 completed

---
+++ 07_projects/1-colorChanger/script.js
@@ -0,0 +1,28 @@
+// using querySelector all to select all the buttons
+const buttons = document.querySelectorAll('.button')
+// console.log(buttons)
+const body = document.querySelector("body")
+
+buttons.forEach( function (btn){
+    console.log(btn)
+    btn.addEventListener('click', function(e){
+        console.log(e)
+        console.log(e.target);
+        if(e.target.id === 'grey'){
+            // body.style.backgroundColor = 'grey'
+            body.style.backgroundColor = e.target.id //its select only id thats also a color name
+        }
+        if(e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id
+        } 
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id
+        } 
+        if(e.target.id === 'purple') {
+            body.style.backgroundColor = 'purple'
+        }
+    })
+})
